88f88b5320c2418d077884ef90734aa1a957e470
jawed <jawedalam5130@gmail.com>
2024-11-29 22:12:04 +0530

    win logic added

--- app.js
+++ app.js
@@ -3,9 +3,26 @@ let compScore = 0;
 
 const choices = document.querySelectorAll(".choice");
 
+
+const genCompChoice = () => {
+    const options = ["rock", "paper", "scissors"];
+    const randIdx = Math.floor(Math.random() * 3);
+    return options[randIdx];
+}
+
+
+const playGame = (userChoice) =>{
+    console.log("User Choice", userChoice);
+    // Generate a Computer Choice
+    const compChoice = genCompChoice();
+    console.log("Computer Choice", compChoice);
+}
+
+
+
 choices.forEach((choice => {
-    console.log(choice);
-    choice.addEventListener("click" , () =>{
-        console.log("Choice was Clicked");
-    })
-}))
+    choice.addEventListener("click" , () => {
+        const userChoice = choice.getAttribute("id");
+        playGame(userChoice);
+    });
+}));
