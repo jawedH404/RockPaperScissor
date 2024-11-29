ac3263998b5811e505bcd29374eda25124f1c63d
jawed <jawedalam5130@gmail.com>
2024-11-29 21:54:52 +0530

    js

---
+++ app.js
@@ -0,0 +1,11 @@
+let userScore = 0;
+let compScore = 0;
+
+const choices = document.querySelectorAll(".choice");
+
+choices.forEach((choice => {
+    console.log(choice);
+    choice.addEventListener("click" , () =>{
+        console.log("Choice was Clicked");
+    })
+}))
