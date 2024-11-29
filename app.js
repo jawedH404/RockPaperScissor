1a387eed908dc6f0a98b6331afdad1fef608843e
jawed <jawedalam5130@gmail.com>
2024-11-29 22:37:58 +0530

    end

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ let userScore = 0;
 let compScore = 0;
 
 const choices = document.querySelectorAll(".choice");
+const msg = document.querySelector("#msg");
+
+const userScorePara = document.querySelector("#user-score");
+const compScorePara = document.querySelector("#comp-score");
+
 
 
 const genCompChoice = () => {
@@ -11,13 +16,52 @@ const genCompChoice = () => {
 }
 
 
-const playGame = (userChoice) =>{
-    console.log("User Choice", userChoice);
-    // Generate a Computer Choice
-    const compChoice = genCompChoice();
-    console.log("Computer Choice", compChoice);
+const drawGame = () => {
+    msg.innerText = "Game Draw! Play Again";
+    msg.style.backgroundColor = "#081b31";
 }
 
+
+const showWinner = (userWin, userChoice,compChoice) => {
+    if(userWin){
+        userScore++;
+        userScorePara.innerText = userScore;
+        msg.innerText = `You win! Your ${userChoice} beats ${compChoice}`;
+        msg.style.backgroundColor = "green";
+    }else{
+        compScore++;
+        compScorePara.innerText = compScore;
+        msg.innerText =  `You lost. ${compChoice} beats your ${userChoice}`;
+        msg.style.backgroundColor = "Red";
+    }
+}
+
+const playGame = (userChoice) =>{
+    // Generate a Computer Choice
+    const compChoice = genCompChoice();
+
+    if(userChoice === compChoice){
+        // Draw Game
+        drawGame();
+    }
+    else {
+        let userWin = true;
+        if(userChoice === "rock"){
+            // scissor, paper
+            userWin = compChoice === "paper" ? false : true;
+        }
+        else if(userChoice === "paper"){
+            // rock, scissor
+            userWin = compChoice === "scissors" ? false : true;
+        }
+        else{
+            // rock, paper
+            userWin = compChoice === "rock" ? false : true;
+        }
+        showWinner(userWin, userChoice,compChoice);
+
+    }
+}
 
 
 choices.forEach((choice => {
